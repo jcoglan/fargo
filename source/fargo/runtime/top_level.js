04235dae560def165b1ebb17ec3e6246ebd8882f
James Coglan <jcoglan@gmail.com>
2011-04-20 23:09:20 +0100

    Implement comparison functions and 'if'.

--- source/fargo/runtime/top_level.js
+++ source/fargo/runtime/top_level.js
@@ -3,14 +3,25 @@ Fargo.Runtime.extend({
     initialize: function() {
       this.callSuper();
       
-      this.define('+', function(a,b) { return a + b })
-      this.define('-', function(a,b) { return a - b })
-      this.define('*', function(a,b) { return a * b })
-      this.define('/', function(a,b) { return a / b })
-      
       this.syntax('define', function(scope, cells) {
         scope.define(cells[0].elements[1].textValue, cells[1].eval(scope));
       });
+      
+      this.syntax('if', function(scope, cells) {
+        var which = cells[0].eval(scope) ? cells[1] : cells[2];
+        return which.eval(scope);
+      });
+      
+      this.define('+', function(a,b) { return a + b });
+      this.define('-', function(a,b) { return a - b });
+      this.define('*', function(a,b) { return a * b });
+      this.define('/', function(a,b) { return a / b });
+      
+      this.define('<',  function(a,b) { return a <  b });
+      this.define('<=', function(a,b) { return a <= b });
+      this.define('>',  function(a,b) { return a >  b });
+      this.define('>=', function(a,b) { return a >= b });
+      this.define('=',  function(a,b) { return a === b });
     }
   })
 });
