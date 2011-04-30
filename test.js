59067a986831818025b8e5a633c73e6caec8873b
James Coglan <jcoglan@gmail.com>
2011-04-30 12:02:08 +0100

    Switch on a few tests that now work.

--- test.js
+++ test.js
@@ -2,24 +2,24 @@ var path   = require('path'),
     assert = require('assert'),
     sys    = require('sys');
 
-JSCLASS_PATH = path.dirname(__filename) + '/build/js.class';
+JSCLASS_PATH = './build/js.class';
 FARGO_PATH   = path.dirname(__filename) + '/build';
 TEST_PATH    = path.dirname(__filename) + '/vendor/heist/test/scheme_tests';
 
 require(JSCLASS_PATH + '/loader');
-require(JSCLASS_PATH + '/core');
-require(JSCLASS_PATH + '/enumerable');
-require(path.dirname(__filename) + '/build/fargo-min');
+JS.require('JS.Test');
+require(FARGO_PATH + '/fargo')
 
-var runtime = new Fargo.Runtime();
+var runtime = new Fargo.Runtime(),
+    test    = new JS.Singleton(JS.Test.Unit.Assertions);
 
 runtime.define('assert', function(truthy) {
-  assert.ok(truthy);
+  test.assert(truthy);
   return true;
 });
 
 runtime.define('assert-equal', function(expected, actual) {
-  assert.equal(expected, actual);
+  test.assertEqual(expected, actual);
   return true;
 });
 
@@ -31,7 +31,7 @@ runtime.syntax('assert-raise', function(scope, cells) {
 // runtime.run(TEST_PATH + '/arithmetic.scm');
 runtime.run(TEST_PATH + '/booleans.scm');
 runtime.run(TEST_PATH + '/closures.scm');
-// runtime.run(TEST_PATH + '/conditionals.scm');
+runtime.run(TEST_PATH + '/conditionals.scm');
 runtime.run(TEST_PATH + '/define_functions.scm');
 // runtime.run(TEST_PATH + '/define_values.scm');
 runtime.run(TEST_PATH + '/delay.scm');
@@ -39,9 +39,9 @@ runtime.run(TEST_PATH + '/delay.scm');
 // runtime.run(TEST_PATH + '/functional.scm');
 // runtime.run(TEST_PATH + '/hygienic.scm');
 // runtime.run(TEST_PATH + '/let.scm');
-// runtime.run(TEST_PATH + '/lists.scm');
+runtime.run(TEST_PATH + '/lists.scm');
 // runtime.run(TEST_PATH + '/macros.scm');
 // runtime.run(TEST_PATH + '/numbers.scm');
 // runtime.run(TEST_PATH + '/protection.scm');
 // runtime.run(TEST_PATH + '/strings.scm');
-// runtime.run(TEST_PATH + '/vectors.scm');
+runtime.run(TEST_PATH + '/vectors.scm');
